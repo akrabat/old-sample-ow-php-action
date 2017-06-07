90255c0e4dd73b1a630f554b8cbe25821baa1662
Rob Allen <rob@akrabat.com>
2017-06-07 12:24:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ echo "Creating Docker container"
 result=`docker build -t $DOCKER_CONTAINER_NAME . | tail -n1`
 echo $result
 
-if [[ ${result} != *"Successfully"* ]];then
+if [[ ${result} != *"Successfully"* ]]; then
     echo "Build failed"
+    echo $result
     exit
 fi
 id=`echo $result | cut -d ' ' -f3`
@@ -20,14 +21,19 @@ id=`echo $result | cut -d ' ' -f3`
 echo "Tagging and pushing to Docker hub"
 docker tag $id $DOCKER_USER/$DOCKER_CONTAINER_NAME
 result=`docker push $DOCKER_USER/$DOCKER_CONTAINER_NAME`
-if [[ ${result} != *"latest"* ]];then
+if [[ ${result} != *"latest"* ]]; then
     echo "Push failed"
     echo $result
     exit
 fi
 
 echo "Updating OpenWhisk action '$OW_ACTION_NAME'"
-wsk action update --docker $OW_ACTION_NAME $DOCKER_USER/$DOCKER_CONTAINER_NAME --web true
+result=`wsk action update --docker $OW_ACTION_NAME $DOCKER_USER/$DOCKER_CONTAINER_NAME --web true`
+if [[ ${result} != *"ok:"* ]]; then
+    echo "Update failed"
+    echo $result
+    exit
+fi
 
 echo "Invoking OpenWhisk action: wsk action invoke -br $OW_ACTION_NAME --param name Everyone"
 wsk action invoke -br $OW_ACTION_NAME --param name Everyone
